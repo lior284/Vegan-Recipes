3f7e61864c8fdd3129d092429554734d52f9b623
lior-284 <lior.biton.284@gmail.com>
2026-03-26 15:19:21 +0200

    Fixed rounded recipes images

--- app/src/main/java/com/example/veganism/RecipeAdapter.kt
+++ app/src/main/java/com/example/veganism/RecipeAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.auth.FirebaseAuth
@@ -53,7 +54,7 @@ class RecipeAdapter(
 
                 Glide.with(holder.image)
                     .load(uri)
-                    .transform(RoundedCorners(radiusPx))
+                    .apply(RequestOptions().transform(CenterCrop(), RoundedCorners(radiusPx)))
                     .into(holder.image)
             }
             .addOnFailureListener {
